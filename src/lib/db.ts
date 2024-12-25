c117785a7e0cfa55a57ba6eb93c3c8859cbeb627
MatheusCanuto07 <matheushenriquecanuto77@gmail.com>
2024-12-25 12:36:54 -0300

    fasdfasd

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,11 @@
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 
-import { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } from "$env/static/private";
 import * as schema from "./schema";
 
 const client = createClient({
-  url: TURSO_DATABASE_URL,
-  authToken: TURSO_AUTH_TOKEN
+  url: "libsql://template-matheuscanuto07.aws-us-east-1.turso.io",
+  authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzUwNzk2MTksImlkIjoiOWE4M2ZkZGEtYzJmOC00MWNmLWJlMjEtMGE0ZTIxMWZlMDQ5In0.TtdK7VK_V34faF4tE2OOTtQQAPf61ehDB2rTqmqf7B7mEVpmNpHGUrRouYmU7AWV_V8yLua_AwLnehtVobNMCQ"
 });
 
 export const db = drizzle(client, {schema: schema});
