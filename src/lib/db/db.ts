e580ad24cb32f7790d7ec66bac4f07403a6f3a63
Matheuscanuto07 <matheushenriquecanuto77@gmail.com>
2025-07-29 23:30:53 -0300

    init

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,14 +1,11 @@
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
-import { config } from 'dotenv';
-
-import * as schema from "./schema";
-
-config({ path: '.env' });
+import {configDataBase} from "../../../config";
+import * as schema from "../db/schema/schema";
 
 const client = createClient({
-  url: "libsql://default-test-matheuscanuto07.turso.io",
-  authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MzgyNTg2MDYsImlkIjoiYWMwYzE2ZDItM2Q0Yy00ZWZhLWJhNzAtMzY4MWI0ODVhNjJiIn0.s0FuHPoWBGpo-f5yE5io9Tnxl--HrQ6PGINmA0RzgYbjl46lH5KTB7tq33XTs5WeqXf7MUTx0qaJjTQz61gzAw"
+  url: configDataBase.databaseUrl || "",
+  authToken: configDataBase.authToken
 });
 
 export const db = drizzle(client, {schema: schema});
